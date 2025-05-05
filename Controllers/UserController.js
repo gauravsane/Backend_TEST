5643821a5db84a73b22e76c8466558f62db6be77
gauravsane <sanegaurav360@gmail.com>
2025-05-05 13:40:04 +0530

    addChanges

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -300,49 +300,190 @@ const getAllWebhooks = async (req, res) => {
 };
 
 const webHooksToSendMessages = async (req, res) => {
-    console.log(JSON.stringify(req.body,null, 2));
-    res.status(200).send('Webhook processed')
-    // if (
-    //   body_params.entry[0] &&
-    //   body_params.entry[0].changes &&
-    //   body_params.entry[0].value.message &&
-    //   body_params.entry[0].value.message[0]
-    // ) {
-    //   let phone_no_id =
-    //     body_params.entry[0].changes[0].vlaue.metadata.phone_number_id;
-    //   let from = body_params.entry[0].changes[0].value.messages.from;
-    //   let msg_body = body_params.entry[0].changes[0].value.message[0].text.body;
+  const { entry } = req.body
 
-    //   axios({
-    //     method: "POST",
-    //     url:
-    //       "https://graph.facebook.com/v22.0/" +
-    //       phone_no_id +
-    //       "/messages?access_token=" +
-    //       token,
-    //     data: {
-    //       messaging_product: "whatsapp",
-    //       to: from,
-    //       text: {
-    //         body: "Hii...I'm Gaurav",
-    //       },
-    //     },
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   });
-    //   return res.status(200).json({
-    //     success: true,
-    //     messsage: "Success",
-    //   });
-    // }
-    // else{
-    //   return res.status(500).json({
-    //     success: false,
-    //     message: "Internal Server Error",
-    //   });
-    // }
+  if (!entry || entry.length === 0) {
+    return res.status(400).send('Invalid Request')
+  }
+
+  const changes = entry[0].changes
+
+  if (!changes || changes.length === 0) {
+    return res.status(400).send('Invalid Request')
+  }
+
+  const statuses = changes[0].value.statuses ? changes[0].value.statuses[0] : null
+  const messages = changes[0].value.messages ? changes[0].value.messages[0] : null
+
+  if (statuses) {
+    // Handle message status
+    console.log(`
+      MESSAGE STATUS UPDATE:
+      ID: ${statuses.id},
+      STATUS: ${statuses.status}
+    `)
+  }
+
+  if (messages) {
+    // Handle received messages
+    if (messages.type === 'text') {
+      if (messages.text.body.toLowerCase() === 'hello') {
+        replyMessage(messages.from, 'Hello. How are you?', messages.id)
+      }
+
+      if (messages.text.body.toLowerCase() === 'list') {
+        sendList(messages.from)
+      }
+
+      if (messages.text.body.toLowerCase() === 'buttons') {
+        sendReplyButtons(messages.from)
+      }
+    }
+
+    if (messages.type === 'interactive') {
+      if (messages.interactive.type === 'list_reply') {
+        sendMessage(messages.from, `You selected the option with ID ${messages.interactive.list_reply.id} - Title ${messages.interactive.list_reply.title}`)
+      }
+
+      if (messages.interactive.type === 'button_reply') {
+        sendMessage(messages.from, `You selected the button with ID ${messages.interactive.button_reply.id} - Title ${messages.interactive.button_reply.title}`)
+      }
+    }
+    
+    console.log(JSON.stringify(messages, null, 2))
+  }
+  
+  res.status(200).send('Webhook processed')
 };
+
+async function replyMessage(to, body, messageId) {
+  await axios({
+    url: 'https://graph.facebook.com/v22.0/674013022456577/messages',
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({
+      messaging_product: 'whatsapp',
+      to,
+      type: 'text',
+      text: {
+        body: body
+      },
+      context: {
+        message_id: messageId
+      }
+    })
+  })
+}
+
+async function sendList(to) {
+  await axios({
+    url: 'https://graph.facebook.com/v22.0/674013022456577/messages',
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({
+      messaging_product: 'whatsapp',
+      to,
+      type: 'interactive',
+      interactive: {
+        type: 'list',
+        header: {
+          type: 'text',
+          text: 'Message Header'
+        },
+        body: {
+          text: 'This is a interactive list message'
+        },
+        footer: {
+          text: 'This is the message footer'
+        },
+        action: {
+          button: 'Tap for the options',
+          sections: [
+            {
+              title: 'First Section',
+              rows: [
+                {
+                  id: 'first_option',
+                  title: 'First option',
+                  description: 'This is the description of the first option'
+                },
+                {
+                  id: 'second_option',
+                  title: 'Second option',
+                  description: 'This is the description of the second option'
+                }
+              ]
+            },
+            {
+              title: 'Second Section',
+              rows: [
+                {
+                  id: 'third_option',
+                  title: 'Third option'
+                }
+              ]
+            }
+          ]
+        }
+      }
+    })
+  })
+}
+
+async function sendReplyButtons(to) {
+  await axios({
+    url: 'https://graph.facebook.com/v22.0/674013022456577/messages',
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({
+      messaging_product: 'whatsapp',
+      to,
+      type: 'interactive',
+      interactive: {
+        type: 'button',
+        header: {
+          type: 'text',
+          text: 'Message Header'
+        },
+        body: {
+          text: 'This is a interactive reply buttons message'
+        },
+        footer: {
+          text: 'This is the message footer'
+        },
+        action: {
+          buttons: [
+            {
+              type: 'reply',
+              reply: {
+                id: 'first_button',
+                title: 'First Button'
+              }
+            },
+            {
+              type: 'reply',
+              reply: {
+                id: 'second_button',
+                title: 'Second Button'
+              }
+            }
+          ]
+        }
+      }
+    })
+  })
+}
+
+
 
 
 // sendMessage1('8652169433','Hello Fawad')
